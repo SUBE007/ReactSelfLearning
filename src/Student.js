af8565b39197590456a0d8bcfca5f1f2404d1ae2
SUBE007 <chaurasiyasube007@gmail.com>
2020-05-26 22:23:19 +0530

    [SUBE007]Created One Component by using function component

---
+++ src/Student.js
@@ -0,0 +1,10 @@
+import React from "react";
+// function Student(){
+//     return <h1> Hello Subedar</h1>;
+// }
+
+const Student=()=>{
+    return <h1> Hello Subedar</h1>;
+}
+
+export default Student;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Student from './Student';
+
 
  
-ReactDOM.render(<App /> ,document.getElementById("root"));
+ 
+ReactDOM.render(<Student /> ,document.getElementById("root"));
 
  
  

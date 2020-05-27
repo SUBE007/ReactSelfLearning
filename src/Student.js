2adf1c2d2cd635b54f3b0a2756c7101d9e4de771
SUBE007 <chaurasiyasube007@gmail.com>
2020-05-27 12:48:45 +0530

    [SUBE]refactor marks props and name props and use of this.props

--- src/Student.js
+++ src/Student.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
  
-const Student=props=>{
-      return(
-            <div>
-              <h1>Hello:{props.name}</h1>
-              <h2> Your Marks:{props.marks}</h2>
-            </div>
-      )
+// const Student=props=>{
+//       return(
+//             <div>
+//               <h1>Hello:{props.name}</h1>
+//               <h2> Your Marks:{props.marks}</h2>
+//             </div>
+//       )
+// }
+
+
+// class Student extends Component{
+//       render(){
+//             return (
+//             <div>
+//                 <h1>Hellow:</h1>
+//             <   h2>Your Marks:</h2>
+//             </div>
+            
+//             );
+//       }
+//     }
+
+class Student extends Component{
+  render(props){
+        return (
+        <div>
+            <h1>Hellow:{this.props.name}</h1>
+        <   h2>Your Marks:{this.props.marks}</h2>
+        </div>
+        
+        );
+  }
 }
+
 export default Student;

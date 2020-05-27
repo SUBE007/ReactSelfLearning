4c6a870e5f92b61c4efd8154bae397d5091765a2
SUBE007 <chaurasiyasube007@gmail.com>
2020-05-27 10:07:51 +0530

    [SUBE]Refactored done many things like method and variable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
  import React from "react";
-  //Simple String
+  //Simple String,variable,function
   //Math Expression
-const element=<h1> Hello {10+40}</h1>;
+// const el="DUBE"
+// const element=<h1> Hello {el}</h1>;
 
+// function show(name){
+//   return name;
+// }
+
+let user={
+  firstname:"subedar"
+}
+const element=<h1> hello {user.firstname}</h1>
  export default element;

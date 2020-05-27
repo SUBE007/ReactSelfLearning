5a73ce766c9a270cf22727655541637a2e0f79d5
SUBE007 <chaurasiyasube007@gmail.com>
2020-05-27 10:25:56 +0530

    [SUBE]Refactored element and Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
- import React from "react";
-  //Simple String,variable,function
-  //Math Expression
-// const el="DUBE"
-// const element=<h1> Hello {el}</h1>;
-
-// function show(name){
-//   return name;
-// }
-
-let user={
-  firstname:"subedar"
-}
-const element=<h1> hello {user.firstname}</h1>
+import React from "react";
+import "./App.css";
+  
+ 
+const element=<h1 className="bg"> hello </h1>
+ 
  export default element;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom';
 import element from './App';
   
-ReactDOM.render( element,document.getElementById("root"));
+ReactDOM.render( element,document.getElementById("root"));//element
 
- 
+ReactDOM.render( <App name="Sube" />,document.getElementById("root"));//component
  
